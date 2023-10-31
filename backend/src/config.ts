28928db7cde11fd38a8372e39f5c730a9935a35a
Jesper <jesper.gronroos@hotmail.com>
2023-10-31 20:55:20 +0200

    added gpt entity extractor endpoint

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,9 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
-const WEAVIATE_DB_URL: string | null = process.env.WEAVIATE_DB_URL || "";
-const WEAVIATE_APIKEY: string | null = process.env.WEAVIATE_APIKEY || "";
-const OPEN_AI_APIKEY: string | null = process.env.OPEN_AI_APIKEY || "";
+const WEAVIATE_DB_URL: string = process.env.WEAVIATE_DB_URL || "";
+const WEAVIATE_APIKEY: string = process.env.WEAVIATE_APIKEY || "";
+const OPEN_AI_APIKEY: string = process.env.OPEN_AI_APIKEY || "";
+const OPEN_AI_API_URL: string = process.env.OPEN_AI_APIKEY || "";
 
 const NODE_ENV: string = process.env.NODE_ENV || "development";
 
@@ -13,5 +14,6 @@ export {
   WEAVIATE_DB_URL,
   WEAVIATE_APIKEY,
   OPEN_AI_APIKEY,
+  OPEN_AI_API_URL,
   NODE_ENV,
 };
